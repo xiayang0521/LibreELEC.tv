6fa8785aee7acb7c19baa2f7bb8330ca05507625
Jonas Karlman <jonas@kwiboo.se>
2019-09-18 17:00:04 +0000

    Rockchip: add mainline linux support

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -38,7 +38,7 @@ else
   PKG_FFMPEG_VDPAU="--disable-vdpau"
 fi
 
-if [ "$PROJECT" = "Rockchip" ]; then
+if [ "$PROJECT" = "Rockchip" -a "$LINUX" = "rockchip-4.4" ]; then
   PKG_PATCH_DIRS+=" rkmpp"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET rkmpp"
   PKG_FFMPEG_RKMPP="--enable-rkmpp --enable-libdrm --enable-version3"
@@ -46,7 +46,7 @@ else
   PKG_FFMPEG_RKMPP="--disable-rkmpp"
 fi
 
-if [ "$PROJECT" = "Allwinner" ]; then
+if [ "$PROJECT" = "Allwinner" -o "$PROJECT" = "Rockchip" ]; then
   PKG_PATCH_DIRS+=" v4l2-request-api"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libdrm systemd" # systemd is needed for libudev
   PKG_FFMPEG_V4L2_REQUEST="--enable-v4l2-request --enable-libudev --enable-libdrm"
